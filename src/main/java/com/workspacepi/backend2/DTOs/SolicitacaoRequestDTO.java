9a984563ebd064512ccb2f2417eaf374740ab94c
gersin <gersonfernandesribeiro48@gmail.com>
2024-09-02 20:57:55 -0300

    Update SolicitacaoRequestDTO.java

--- src/main/java/com/workspacepi/backend2/DTOs/SolicitacaoRequestDTO.java
+++ src/main/java/com/workspacepi/backend2/DTOs/SolicitacaoRequestDTO.java
@@ -1,12 +1,12 @@
-package com.workspacepi.backend2.DTOs;
-
-// Importa a classe Date para trabalhar com datas
-import java.util.Date;
-
-// Define um registro (record) chamado SolicitacaoRequestDTO com os campos especificados
-public record SolicitacaoRequestDTO(
-        String cpf,
-        Date data,
-        String destinatario) {
-    // O record automaticamente cria construtor, getters, equals, hashCode e toString
-}
+//package com.workspacepi.backend2.DTOs;
+//
+//// Importa a classe Date para trabalhar com datas
+//import java.util.Date;
+//
+//// Define um registro (record) chamado SolicitacaoRequestDTO com os campos especificados
+//public record SolicitacaoRequestDTO(
+//        String cpf,
+//        Date data,
+//        String destinatario) {
+//    // O record automaticamente cria construtor, getters, equals, hashCode e toString
+//}
